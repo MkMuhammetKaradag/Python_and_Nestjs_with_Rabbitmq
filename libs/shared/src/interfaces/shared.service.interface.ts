e72f85d75c39a5f3f5e59b014debe7c6bc559dc2
MkMuhammetKaradag <mk.muhammetkaradag@gmail.com>
2024-08-22 09:13:10 +0300

    (shared library) Created a configuration for RabbitMQ

---
+++ libs/shared/src/interfaces/shared.service.interface.ts
@@ -0,0 +1,6 @@
+import { RmqContext, RmqOptions } from '@nestjs/microservices';
+
+export interface SharedServiceInterface {
+  getRmqOptions(queueName: string): RmqOptions;
+  acknowledgeMessage(context: RmqContext): void;
+}
---
+++ libs/shared/src/services/shared.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { RmqContext, RmqOptions, Transport } from '@nestjs/microservices';
+import { SharedServiceInterface } from '../interfaces/shared.service.interface';
+
+@Injectable()
+export class SharedService implements SharedServiceInterface {
+  constructor(private readonly configService: ConfigService) {}
+
+  getRmqOptions(queueName: string): RmqOptions {
+    const USER = this.configService.get<string>('RABBITMQ_USER');
+    const PASS = this.configService.get<string>('RABBITMQ_PASS');
+    const HOST = this.configService.get<string>('RABBITMQ_HOST');
+    const QUEUE = this.configService.get<string>(`RABBITMQ_${queueName}_QUEUE`);
+    // const URI = this.configService.get<string>('RABBITMQ_URI');
+
+    return {
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${USER}:${PASS}@${HOST}`],
+        // urls: [URI],
+        queue: QUEUE,
+        noAck: false,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    };
+  }
+  acknowledgeMessage(context: RmqContext): void {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+  }
+}
